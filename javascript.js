dd7cc551d7e9209ee6122de9ecb93c495f165568
Leandro Magnotti <leandro.magnotti@gmail.com>
2023-06-18 10:45:39 -0300

    loadImageBy2Functs

--- javascript.js
+++ javascript.js
@@ -1,21 +1,21 @@
-// function mostrarImagen() {
-//     const btn = document.querySelector(".btn");
-//     const imagen = document.getElementById("img1");
+function mostrarImagen() {
+    const btn = document.querySelector(".btn");
+    const imagen = document.getElementById("img1");
 
-//     btn.addEventListener('click', () => {
-//         promesa = new Promise((resolve, reject) => {
-//             imagen.src = "images/johnwick4.jpg",
-//             imagen.addEventListener('load', () => {
-//                 resolve(console.log('bien ahí pedazo de hijo de puta!!'))
-//             });
-//             imagen.addEventListener('error', () => {
-//                 reject(console.log('sos la cara de la verga'))
-//             });
-//         });
-//     });
-// }
+    btn.addEventListener('click', () => {
+        promesa = new Promise((resolve, reject) => {
+            imagen.src = "images/johnwick4.jpg",
+            imagen.addEventListener('load', () => {
+                resolve(console.log('La imagen se cargó correctamente'))
+            });
+            imagen.addEventListener('error', () => {
+                reject(console.log('Error: no se pudo cargar la imagen'))
+            });
+        });
+    });
+}
 
-// mostrarImagen();
+mostrarImagen();
 
 
 function cargarImagen(url) {
