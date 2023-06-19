a1ce726d941e65c3f7caab50d3bd6f14f84d03e8
Leandro Magnotti <leandro.magnotti@gmail.com>
2023-06-19 13:21:18 -0300

    TryingAPromise

--- javascript.js
+++ javascript.js
@@ -12,66 +12,60 @@ function aceptarRegistro() {
   const email = document.getElementById("email").value;
   const contrasena = document.getElementById("contrasena").value;
 
-  form.addEventListener("submit", async (event) => {
+  form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const regExName = /^[a-zA-Z]{1,}$/;
     const regExLastName = /^[a-zA-Z]{1,}$/;
     const regExEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const regExPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    
+    promesa = new Promise( async (resolve, reject) => {
+      regExName.test(nombre) && regExLastName.test(apellido) && regExEmail.test(email) && regExPassword.test(contrasena) ? 
+        resolve((await Swal.fire({
+              title: "Estamos procesando tus datos...",
+              color: "lightblue",
+              showConfirmButton: false,
+              imageUrl: "/images/blue-black-load.gif",
+              imageWidth: 400,
+              imageAlt: "Custom image",
+              background: "black",
+              timer: 3000,
+            }))
+        ) :
+        reject(console.log("Error! no es por acá"));
+    });
+  })
 
-    regExName.test(nombre) &&
-    regExLastName.test(apellido) &&
-    regExEmail.test(email) &&
-    regExPassword.test(contrasena)
-      ? (await Swal.fire({
-          title: "Estamos procesando tus datos...",
-          color: "lightblue",
-          showConfirmButton: false,
-          imageUrl: "/images/blue-black-load.gif",
-          imageWidth: 400,
-          imageAlt: "Custom image",
-          background: "black",
-          timer: 3000,
-        }),
-        Swal.fire({
-          icon: "success",
-          title: `Excelente, ${nombre}!`,
-          text: "Tu registro se ha completado exitosamente!",
-        }))
-      : !regExName.test(nombre)
-      ? Swal.fire({
-          icon: "error",
-          title: "Nombre inválido",
-          text: "Tu nombre debe estar formado sólo por letras y no contener espacios en blanco",
-        })
-      : !regExLastName.test(apellido)
-      ? Swal.fire({
-          icon: "error",
-          title: "Apellido inválido",
-          text: "Tu apellido debe estar formado sólo por letras y no contener espacios en blanco",
-        })
-      : !regExEmail.test(email)
-      ? Swal.fire({
-          icon: "error",
-          title: "E-mail inválido",
-          text: "Introduce un e-mail válido",
-        })
-      : !regExPassword.test(contrasena)
-      ? Swal.fire({
-          icon: "error",
-          title: "Contraseña inválida",
-          text: "La contraseña debe contener al menos un dígito, una letra minúscula, una mayúscula y estar formada por al menos 8 caracteres",
-        })
-      : Swal.fire({
-          icon: "error",
-          title: "No pudimos completar el registro",
-          text: "Se produjo un error inesperado",
-        });
-  });
+  .then(Swal.fire({
+    icon: "success",
+    title: `Excelente, ${nombre}!`,
+    text: "Tu registro se ha completado exitosamente!",
+  }))
+
+  .catch(!regExName.test(nombre) ? 
+    mostrarError("Nombre inválido", "Tu nombre debe estar formado sólo por letras y no contener espacios en blanco")
+    : !regExLastName.test(apellido) ? 
+    mostrarError("Apellido inválido", "Tu apellido debe estar formado sólo por letras y no contener espacios en blanco")
+    : !regExEmail.test(email) ? 
+    mostrarError("E-mail inválido", "Introduce un e-mail válido") 
+    : !regExPassword.test(contrasena) ? 
+    mostrarError("Contraseña inválida", "La contraseña debe contener al menos un dígito, una letra minúscula, una mayúscula y estar formada por al menos 8 caracteres")
+    : mostrarError("No pudimos completar el registro", "Se produjo un error inesperado")
+  );
+}
+
+function mostrarError(titulo, texto) {
+  Swal.fire({
+    icon: "error",
+    title: titulo,
+    text: texto,
+  })
 }
 
 submit();
+
+
 
 function mostrarImagen() {
   const btn = document.querySelector(".btn");
