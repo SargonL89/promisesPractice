4827e315e3c8a238c264bc033c1216b8de2aaf8d
Leandro Magnotti <leandro.magnotti@gmail.com>
2023-06-20 19:25:12 -0300

    formPromiseWorking

--- javascript.js
+++ javascript.js
@@ -20,9 +20,9 @@ function aceptarRegistro() {
     const regExEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const regExPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
     
-    promesa = new Promise( async (resolve, reject) => {
+    promesa = new Promise((resolve, reject) => {
       regExName.test(nombre) && regExLastName.test(apellido) && regExEmail.test(email) && regExPassword.test(contrasena) ? 
-        resolve((await Swal.fire({
+        (Swal.fire({
               title: "Estamos procesando tus datos...",
               color: "lightblue",
               showConfirmButton: false,
@@ -31,28 +31,29 @@ function aceptarRegistro() {
               imageAlt: "Custom image",
               background: "black",
               timer: 3000,
-            }))
-        ) :
-        reject(console.log("Error! no es por acá"));
+        }))
+        .then(() => {
+          resolve();
+          (Swal.fire({
+            icon: "success",
+            title: `Excelente, ${nombre}!`,
+            text: "Tu registro se ha completado exitosamente!",
+          }))
+        })
+        .catch((error) => {
+          reject(error)
+        })
+      : !regExName.test(nombre) ? 
+      mostrarError("Nombre inválido", "Tu nombre debe estar formado sólo por letras y no contener espacios en blanco")
+      : !regExLastName.test(apellido) ? 
+      mostrarError("Apellido inválido", "Tu apellido debe estar formado sólo por letras y no contener espacios en blanco")
+      : !regExEmail.test(email) ? 
+      mostrarError("E-mail inválido", "Introduce un e-mail válido") 
+      : !regExPassword.test(contrasena) ? 
+      mostrarError("Contraseña inválida", "La contraseña debe contener al menos un dígito, una letra minúscula, una mayúscula y estar formada por al menos 8 caracteres")
+      : reject(new Error("Error de validación"))
     });
   })
-
-  .then(Swal.fire({
-    icon: "success",
-    title: `Excelente, ${nombre}!`,
-    text: "Tu registro se ha completado exitosamente!",
-  }))
-
-  .catch(!regExName.test(nombre) ? 
-    mostrarError("Nombre inválido", "Tu nombre debe estar formado sólo por letras y no contener espacios en blanco")
-    : !regExLastName.test(apellido) ? 
-    mostrarError("Apellido inválido", "Tu apellido debe estar formado sólo por letras y no contener espacios en blanco")
-    : !regExEmail.test(email) ? 
-    mostrarError("E-mail inválido", "Introduce un e-mail válido") 
-    : !regExPassword.test(contrasena) ? 
-    mostrarError("Contraseña inválida", "La contraseña debe contener al menos un dígito, una letra minúscula, una mayúscula y estar formada por al menos 8 caracteres")
-    : mostrarError("No pudimos completar el registro", "Se produjo un error inesperado")
-  );
 }
 
 function mostrarError(titulo, texto) {
