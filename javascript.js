144cefc059f958ae54df9462663f65c1f01f0f8c
Leandro Magnotti <leandro.magnotti@gmail.com>
2023-07-05 22:26:26 -0300

    Working, but...

--- javascript.js
+++ javascript.js
@@ -1,49 +1,40 @@
-function submit() {
-  const registrarse = document.getElementById("registrationForm");
-  registrarse.addEventListener("click", () => {
-    aceptarRegistro();
-  });
-}
+const regExName = /^[a-zA-Z]{1,}$/;
+const regExLastName = /^[a-zA-Z]{1,}$/;
+const regExEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const regExPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
-function aceptarRegistro() {
+function submit() {
   const form = document.getElementById("registrationForm");
-  const nombre = document.getElementById("nombre").value;
-  const apellido = document.getElementById("apellido").value;
-  const email = document.getElementById("email").value;
-  const contrasena = document.getElementById("contrasena").value;
 
   form.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
+    const contrasena = document.getElementById("contrasena").value;
 
-    const regExName = /^[a-zA-Z]{1,}$/;
-    const regExLastName = /^[a-zA-Z]{1,}$/;
-    const regExEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const regExPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-    
-    promesa = new Promise((resolve, reject) => {
-      regExName.test(nombre) && regExLastName.test(apellido) && regExEmail.test(email) && regExPassword.test(contrasena) ? 
-        (Swal.fire({
-              title: "Estamos procesando tus datos...",
-              color: "lightblue",
-              showConfirmButton: false,
-              imageUrl: "/images/blue-black-load.gif",
-              imageWidth: 400,
-              imageAlt: "Custom image",
-              background: "black",
-              timer: 3000,
-        }))
-        .then(() => {
-          resolve();
-          (Swal.fire({
-            icon: "success",
-            title: `Excelente, ${nombre}!`,
-            text: "Tu registro se ha completado exitosamente!",
-          }))
-        })
-        .catch((error) => {
-          reject(error)
-        })
-      : !regExName.test(nombre) ? 
+    event.preventDefault(),
+    promesa()
+    .then(() => {
+      Swal.fire({
+        title: "Estamos procesando tus datos...",
+        color: "lightblue",
+        showConfirmButton: false,
+        imageUrl: "/images/blue-black-load.gif",
+        imageWidth: 400,
+        imageAlt: "Custom image",
+        background: "black",
+        timer: 3000,
+      })
+      .then(() => {
+        Swal.fire({
+          icon: "success",
+          title: "Excelente!",
+          text: "Tu registro se ha completado exitosamente!",
+          })
+      })
+    })
+    .catch(() => {
+      !regExName.test(nombre) ? 
       mostrarError("Nombre inválido", "Tu nombre debe estar formado sólo por letras y no contener espacios en blanco")
       : !regExLastName.test(apellido) ? 
       mostrarError("Apellido inválido", "Tu apellido debe estar formado sólo por letras y no contener espacios en blanco")
@@ -51,9 +42,9 @@ function aceptarRegistro() {
       mostrarError("E-mail inválido", "Introduce un e-mail válido") 
       : !regExPassword.test(contrasena) ? 
       mostrarError("Contraseña inválida", "La contraseña debe contener al menos un dígito, una letra minúscula, una mayúscula y estar formada por al menos 8 caracteres")
-      : reject(new Error("Error de validación"))
-    });
-  })
+      : mostrarError("Ocurrió un error inesperado")
+    })
+  });
 }
 
 function mostrarError(titulo, texto) {
@@ -64,7 +55,25 @@ function mostrarError(titulo, texto) {
   })
 }
 
-submit();
+function promesa() {
+  const nombre = document.getElementById("nombre").value;
+  const apellido = document.getElementById("apellido").value;
+  const email = document.getElementById("email").value;
+  const contrasena = document.getElementById("contrasena").value;
+
+  return new Promise((resolve, reject) => {
+    regExName.test(nombre) && regExLastName.test(apellido) && regExEmail.test(email) && regExPassword.test(contrasena) ? 
+    resolve('Promesa resuelta')
+    : reject('Promesa rechazada') 
+  })
+}
+
+submit()
+
+
+
+
+
 
 
 
